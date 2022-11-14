38a290540429646a5aa04d1a3e9f5de0e705d3b2
rote1400 <rote1400@student.miun.se>
2022-11-14 20:02:25 +0100

    express get

--- restApi.js
+++ restApi.js
@@ -1,1 +1,8 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('Hello World');
+});
+
+app.listen(3030, () => console.log('Listening on port 3030...'));
