44aee8861912c630f3d1e4de7ad3c658262c90cc
rote1400 <rote1400@student.miun.se>
2022-11-14 23:44:28 +0100

    array

--- restApi.js
+++ restApi.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
 
+const cities = [
+    { id: 1, name: `Sofia` },
+    { id: 2, name: `Pekin` },
+    { id: 3, name: `Stockholm` }
+];
+
 app.get('/', (request, response) => {
     response.send('Hello World');
 });
 
 app.get('/api/cities', (request, response) => {
-    response.send([1, 2, 3]);
+    response.send(cities);
+});
+
+app.get(`/api/cities/:id`, (request, response) => {
+    const city = cities.find(city => city.id === parseInt(request.params.id));
+    if (!city) response.status(404).send(`City not found`);
+    response.send(city);
 });
 
 // Port
