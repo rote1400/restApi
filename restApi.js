577e4b93bfa6b4f1afd4efbbf3e9a01c2cede5fd
rote1400 <rote1400@student.miun.se>
2022-11-14 23:42:48 +0100

    get

--- restApi.js
+++ restApi.js
@@ -5,4 +5,10 @@ app.get('/', (request, response) => {
     response.send('Hello World');
 });
 
-app.listen(3030, () => console.log('Listening on port 3030...'));
+app.get('/api/cities', (request, response) => {
+    response.send([1, 2, 3]);
+});
+
+// Port
+const port = process.env.port || 3030;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
